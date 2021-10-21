0c97f09086a9fd9b4e965afbdfaa58498127a2d0
JananiVijayakumar <vsvprema@gmail.com>
2021-10-21 23:17:23 +0530

    Employee Monthly Wage

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class EmployeeWageProgram {
     public static void main(String[] args) {
         Attendance attendance = new Attendance();
-        attendance.DailyWageCheck();
+        attendance.MonthlyWageCheck();
     }
 }
 class Attendance{
@@ -13,25 +13,31 @@ class Attendance{
     int IS_FULL_TIME = 8;
     int IS_PART_TIME = 4;
     int DailyWage = 0;
+    int MonthlyWage = 0;
+    int NO_OF_WORKING_DAYS = 20;
 
     int EmployeePresent(int a) {
         Random r = new Random();
         int EmpCheck = r.nextInt(a);
         return EmpCheck;
     }
-    void DailyWageCheck() {
+    void MonthlyWageCheck() {
         int EmpCheck = EmployeePresent(2);
         switch (EmpCheck) {
-            case IS_PRESENT:{
+            case 1 :{
                 System.out.println("Employee is present");
                 DailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+                MonthlyWage = NO_OF_WORKING_DAYS * DailyWage;
                 break;
             }
-            case IS_ABSENT:{
+            case 2 :{
                 System.out.println("Employee is Absent");
                 DailyWage = DailyWage;
+                MonthlyWage = MonthlyWage;
                 break;
             }
-            System.out.println("Daily Wage : " + DailyWage);
         }
+        System.out.println("Daily Wage : " + DailyWage);
+        System.out.println("Monthly Wage : " + MonthlyWage);
     }
+}
